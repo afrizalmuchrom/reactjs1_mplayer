57515c2d8afd6e762cccf13456636b4e76b0d8e0
iam <afrizalmuchrom@gmail.com>
2020-07-26 17:23:13 +0700

    Updates

---
+++ precache-manifest.decc541f55ead6ac909b6250e65e1f42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0949befa5d9ad8c2bde48534c2fb95dd",
+    "url": "/reactjs1_mplayer/index.html"
+  },
+  {
+    "revision": "c31f8eac5aa029e35c05",
+    "url": "/reactjs1_mplayer/static/css/2.c4e79553.chunk.css"
+  },
+  {
+    "revision": "35266b71d59b2ad0921a",
+    "url": "/reactjs1_mplayer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c31f8eac5aa029e35c05",
+    "url": "/reactjs1_mplayer/static/js/2.0b943644.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/reactjs1_mplayer/static/js/2.0b943644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35266b71d59b2ad0921a",
+    "url": "/reactjs1_mplayer/static/js/main.0f6b2684.chunk.js"
+  },
+  {
+    "revision": "d12a82e97f911a2969d9",
+    "url": "/reactjs1_mplayer/static/js/runtime-main.c0d63c39.js"
+  }
+]);
